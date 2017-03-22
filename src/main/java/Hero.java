5e3cf7e93eed8438bd1de39499cf0c138cabb100
Shane Stafford and Xi Xia <xi@>
2017-03-22 15:35:08 -0700

    finalize v1 of hero class

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Hero {
   private String mHeroName;
@@ -6,6 +8,9 @@ public class Hero {
   private int mAttackPower;
   private int mChanceToHit;
   private int mDefenseRating;
+  private static List<Hero> instances = new ArrayList<Hero>();
+  private int mHeroId;
+  private boolean mHeroDefeated;
 
   public Hero(String heroName) {
     Random randomAttribute = new Random();
@@ -14,6 +19,9 @@ public class Hero {
     mAttackPower = randomAttribute.nextInt(16) + 15;
     mChanceToHit = 101;
     mDefenseRating = randomAttribute.nextInt(16) + 10;
+    instances.add(this);
+    mHeroId = instances.size();
+    mHeroDefeated = false;
   }
 
   public String getHeroName() {
@@ -35,5 +43,26 @@ public class Hero {
   public int getDefenseRating() {
     return mDefenseRating;
   }
+
+  public static List<Hero> getHeroList() {
+    return instances;
+  }
+
+  public static void clearHero() {
+    instances.clear();
+  }
+
+  public int getHeroId() {
+    return mHeroId;
+  }
+
+  public static Hero findHero(int heroId) {
+    return instances.get(heroId - 1);
+  }
+
+  public boolean getDefeatStatus() {
+    return mHeroDefeated;
+  }
+
 
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -2,10 +2,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HeroTest {
-  // @After
-  // public void tearDown() {
-  //   Hero.clear();
-  // }
+  @After
+  public void tearDown() {
+    Hero.clearHero();
+  }
 
   @Test
   public void createsNewHero_True() {
@@ -26,6 +26,31 @@ public class HeroTest {
     assertEquals(true, newHero.getAttackPower()>14);
     assertEquals(101, newHero.getChanceToHit());
     assertEquals(true, newHero.getDefenseRating()>9);
+  }
+
+  @Test
+  public void heroList_CreatesArray_1() {
+    Hero newHero = new Hero("Frank");
+    assertEquals(1, newHero.getHeroList().size());
+  }
+
+  @Test
+  public void heroList_GetsID_1() {
+    Hero newHero = new Hero("Frank");
+    assertEquals(1, newHero.getHeroId());
+  }
+
+  @Test
+  public void heroList_FindsHero_newHero2() {
+    Hero newHero = new Hero("Frank");
+    Hero newHero2 = new Hero("John");
+    assertEquals(newHero2, Hero.findHero(newHero2.getHeroId()));
+  }
+
+  @Test
+  public void heroDefeat_False() {
+    Hero newHero = new Hero("Frank");
+    assertEquals(false, newHero.getDefeatStatus());
   }
 
 }
