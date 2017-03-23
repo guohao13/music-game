3ac44b3c310e552f3c0b71abccd3c00860ad17db
Shane Stafford and Xi Xia <xi@>
2017-03-23 13:56:21 -0700

    update UI with adding band members

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -29,6 +29,14 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("bands/:id/members/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Band band = Band.findBand(Integer.parseInt(request.params(":id")));
+      model.put("band", band);
+      model.put("template", "templates/members-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     post("/bands", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       String bandName1 = request.queryParams("bandName1");
@@ -39,6 +47,31 @@ public class App {
       model.put("template", "templates/bands.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("/bands:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Band band = Band.findBand(Integer.parseInt(request.queryParams("bandId")));
+      String leadSinger = request.queryParams("leadSinger");
+      String drummer = request.queryParams("drummer");
+      String bassist = request.queryParams("bassist");
+      Hero newHero1 = new Hero(leadSinger);
+      Hero newHero2 = new Hero(drummer);
+      Hero newHero3 = new Hero(bassist);
+      band.addHero(newHero1);
+      band.addHero(newHero2);
+      band.addHero(newHero3);
+      model.put("band", band);
+      model.put("template", "templates/band.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // post("/bands/:id", (request, response) -> {
+    // //   Map<String, Object> model = new HashMap<String, Object>();
+    // //   Band band = Band.findBand(Integer.parseInt(request.params(":id")));
+    // //   model.put("band", band);
+    // //   model.put("template", "templates/band.vtl");
+    // //   return new ModelAndView(model, layout);
+    // // }, new VelocityTemplateEngine());
 
   }
 }
