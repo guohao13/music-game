2971d2c9b27f667bff4ab7b9fe0ef8ab8b421a2b
Shane Stafford and Xi Xia <shane@>
2017-03-23 16:00:22 -0700

    allow selection of figther for arena.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,11 +3,16 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    List<Band> bands = new ArrayList<Band>();
+    List<Hero> heroes = new ArrayList<Hero>();
+
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
@@ -45,6 +50,17 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/bands/:id1/members/:id2", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Band band = Band.findBand(Integer.parseInt(request.params(":id1")));
+      Hero member = Hero.findHero(Integer.parseInt(request.params(":id2")));
+      model.put("band", band);
+      model.put("member", member);
+      model.put("template", "templates/hero.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
     post("/bands", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       String bandName1 = request.queryParams("bandName1");
@@ -68,10 +84,32 @@ public class App {
       band.addHero(newHero1);
       band.addHero(newHero2);
       band.addHero(newHero3);
+      System.out.println("Size of band-" + band.getBandMembers());
       model.put("band", band);
       model.put("template", "templates/band.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("/arena", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      if (bands.size() < 2) {
+        Band band = Band.findBand(Integer.parseInt(request.queryParams("bandId")));
+        Hero hero = Hero.findHero(Integer.parseInt(request.queryParams("memberId")));
+        bands.add(band);
+        heroes.add(hero);
+        model.put("bands", Band.getBandInstances());
+        model.put("template", "templates/bands.vtl");
+      }
+      if (bands.size() == 2) {
+        Game newGame = new Game (bands.get(0),bands.get(1), heroes.get(0), heroes.get(1));
+        model.put("template", "templates/arena.vtl");
+      }
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+
+
 
     // post("/bands/:id", (request, response) -> {
     // //   Map<String, Object> model = new HashMap<String, Object>();
